e2263c92486dd2f026d3e27792012855be526d40
Michael Johansen <michaeljohansen83@gmail.com>
2014-01-10 13:30:42 +0100

    removed unnecessary annotation

--- web-app/src/main/java/no/spring/web/Counter.java
+++ web-app/src/main/java/no/spring/web/Counter.java
@@ -1,13 +1,10 @@
 package no.spring.web;
 
-import org.springframework.stereotype.Component;
-
 /**
  * User: Michael Johansen
  * Date: 10.01.14
  * Time: 13:08
  */
-@Component
 public class Counter {
     private int count = 0;
 
